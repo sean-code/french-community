ce801d3c59cefdb12e32459908a9d1883d43f99a
sean-code <john.nganga@student.moringaschool.com>
2023-02-14 14:56:06 +0300

    Create routes and paths

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import NavBar from './components/navbar/navbar';
 import './App.css';
+import Home from './components/home/home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <NavBar />
-    </>
+      <Routes>
+        <Route path='/#' element={<Home />} />
+        <Route></Route>
+        <Route></Route>
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
+
+
+{/* <Router>
+<NavBar />
+<Routes>
+  <Route path='/#' element={<Home />} />
+  <Route></Route>
+  <Route></Route>
+</Routes>
+</Router> */}
