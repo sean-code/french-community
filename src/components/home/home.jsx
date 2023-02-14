d68030a69b9423033d08ef417e4d0992cd15bd2e
sean-code <john.nganga@student.moringaschool.com>
2023-02-14 15:09:01 +0300

    Create carousel

---
+++ src/components/home/home.jsx
@@ -0,0 +1,14 @@
+
+// import SimpleSlider from './carousel';
+import Slider from './carousel';
+import './home.css';
+
+const Home = () => {
+  return(
+    <>
+     <Slider />
+    </>
+  )
+}
+
+export default Home;
