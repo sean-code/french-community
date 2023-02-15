4ab3c003b8e96cca28fcf9c9ee88bb0a229c18bb
sean-code <john.nganga@student.moringaschool.com>
2023-02-15 12:25:47 +0300

    Change h2 to h1 to maximize visibility

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -7,11 +7,11 @@ import './home.css';
 const Home = () => {
   return(
     <div>
-      <section>
+      <section style={{marginBottom:'5%'}}>
         <Slider />
       </section>
-      <section>
-        <h2>Why Choose Us</h2>
+      <section className='section-2'>
+        <h1>WHY CHOOSE US?</h1>
         <WhyCard />
       </section>
     </div>
