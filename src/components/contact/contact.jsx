6b7c9127e61812c18c33df967d58746313c6ca33
sean-code <john.nganga@student.moringaschool.com>
2023-02-18 23:14:08 +0300

    Create the contact form

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,8 +1,24 @@
-
+import './contact.css';
 
 const Contact = () => {
     return(
         <>
+            <div className='contact-container'>
+                <form action="/" className="decor">
+                    <div className="form-left-decoration"></div>
+                    <div className="form-right-decoration"></div>
+                    <div className="circle"></div>
+                    <div className="form-inner">
+                        <h1>Contact us</h1>
+                        <input type="text" placeholder="First Name" />
+                        <input type="text" placeholder="Last Name" />
+                        <input type="email" placeholder="Email" />
+                        <input type="number" placeholder="Phone Number" />
+                        <textarea placeholder="Your Message..." rows="5"></textarea>
+                        <button type="submit" href="/">Send</button>
+                    </div>
+                </form>
+            </div>
         </>
     )
 };
