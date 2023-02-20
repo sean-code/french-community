991423185e6d0c95e0159dd34dca1b933439c9f9
sean-code <john.nganga@student.moringaschool.com>
2023-02-20 16:24:35 +0300

    Create data file

---
+++ src/components/services/service-items.jsx
@@ -0,0 +1,39 @@
+
+export const data = () =>[
+    {
+        id:1,
+        img: "https://media.istockphoto.com/id/1169984447/photo/do-you-speak-french-written-on-board-france-flag-standing-in-box-language.jpg?b=1&s=170667a&w=0&k=20&c=6pDDElK2baw4JLXFD86NaFagBRVdWUBd4zmYoS6qkFs=",
+        title: "Translation Services",
+        content: "We offer French Translation Services in the following language pairs: French to English Translation Services English to French, Translation Services French to Swahili translation Services, and Swahili to French translation Services"
+    },
+    {
+        id:2,
+        img: "https://media.istockphoto.com/id/1169984447/photo/do-you-speak-french-written-on-board-france-flag-standing-in-box-language.jpg?b=1&s=170667a&w=0&k=20&c=6pDDElK2baw4JLXFD86NaFagBRVdWUBd4zmYoS6qkFs=",
+        title: "Team Building and Events Management",
+        content: "We facilitate team building workshops to create positive relationships and synergy within french teaching schools, organizations/institutions. This makes sure you are motivated and promotes growth and attitude change. Our approach is mainly based on experiental learning and real life case studies of the French Language"
+    },
+    {
+        id:3,
+        img: "https://media.istockphoto.com/id/1169984447/photo/do-you-speak-french-written-on-board-france-flag-standing-in-box-language.jpg?b=1&s=170667a&w=0&k=20&c=6pDDElK2baw4JLXFD86NaFagBRVdWUBd4zmYoS6qkFs=",
+        title: "French Tutoring (Remote and Physical)",
+        content: "We have compassionate and dedicated french teachers ready to deliver fully and quality knowledge to any of our students. They all possess well-versed computer literacy hence making online learning seamless. We use a variety of teaching methods to help our students master the content."
+    },
+    {
+        id:4,
+        img: "https://media.istockphoto.com/id/1169984447/photo/do-you-speak-french-written-on-board-france-flag-standing-in-box-language.jpg?b=1&s=170667a&w=0&k=20&c=6pDDElK2baw4JLXFD86NaFagBRVdWUBd4zmYoS6qkFs=",
+        title: "Customized Call Center Outsourcing Services",
+        content: "We offer Customer Care Services in French Organizations, a first point of contact for calls from our English speaking customers and the primary point of contact for French speaking customers."
+    },
+    {
+        id:5,
+        img: "https://media.istockphoto.com/id/1169984447/photo/do-you-speak-french-written-on-board-france-flag-standing-in-box-language.jpg?b=1&s=170667a&w=0&k=20&c=6pDDElK2baw4JLXFD86NaFagBRVdWUBd4zmYoS6qkFs=",
+        title:"Mentorship Programme",
+        content:"Our Mentorship Development Program focuses on self and team development. The program can be customized to suit your company/institution/individual needs in line with your stated objectives."
+    },
+    {
+        id:6,
+        img: "https://media.istockphoto.com/id/1169984447/photo/do-you-speak-french-written-on-board-france-flag-standing-in-box-language.jpg?b=1&s=170667a&w=0&k=20&c=6pDDElK2baw4JLXFD86NaFagBRVdWUBd4zmYoS6qkFs=",
+        title:"Coaching and Mentoring",
+        content:"We offer a range of coaching and mentoring programs for your aimed at candidates, French career opportunities, French community networking et cetera."
+    }
+]
