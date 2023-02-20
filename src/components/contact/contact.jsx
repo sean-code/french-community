9ef64b371f54402a7aa19ae834eb25d948a35fc1
sean-code <john.nganga@student.moringaschool.com>
2023-02-20 17:12:20 +0300

    Add a paragraph and render media icons

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,10 +1,23 @@
 import './contact.css';
+import twitter from './assets/twitter.svg';
+import instagram from './assets/instagram.svg';
+import facebook from './assets/facebook.svg';
 
 const Contact = () => {
     return(
         <>
             <div className='contact-section1'>
                 <h1>Contact Us</h1>
+                <h2 style={{textAlign:'center', marginTop:'20px'}}>We'd love to hear from you</h2>
+                <p style={{textAlign:'center', marginTop:'10px', fontSize:'1.2rem'}}>
+                    Have you any question in mind or want to enquire?<br/>
+                    Please feel free to contact us through the form or the following platforms
+                </p>
+                <div className="media-icons" style={{margin:"2% 43% 2% 43%"}}>
+                        <a href="#/"><img src={twitter} alt='twitter' /></a>
+                        <a href="#/"><img src={instagram} alt='instagram' /></a>
+                        <a href="#/"><img src={facebook} alt='facebook' /></a>
+                </div>
             
 
             </div>
