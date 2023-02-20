03e18a79d56e8e989a283a9e7eb8ad4e85184ad0
sean-code <john.nganga@student.moringaschool.com>
2023-02-20 18:48:38 +0300

    Create members content

--- src/components/team/team.jsx
+++ src/components/team/team.jsx
@@ -2,6 +2,34 @@
 const Team = () => {
     return(
         <>
+            <div class="wrapper">
+                <h1>Our Team</h1>
+                <div class="team">
+                    <div class="team_member">
+                        <div class="team_img">
+                            <img src="https://i.imgur.com/2pGPLrl.png" alt="Team_image" />
+                        </div>
+                            <h3>Paul Doe</h3>
+                            <p class="role">UI developer</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quaerat tempora, voluptatum quas facere dolorum aut cumque nihil nulla harum nemo distinctio quam blanditiis dignissimos.</p>
+                    </div>
+                    <div class="team_member">
+                        <div class="team_img">
+                            <img src="https://i.imgur.com/jQj1I8E.png" alt="Team_image" />
+                        </div>
+                        <h3>Rosie Meg</h3>
+                            <p class="role">Tester</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quaerat tempora, voluptatum quas facere dolorum aut cumque nihil nulla harum nemo distinctio quam blanditiis dignissimos.</p></div>
+                        <div class="team_member">
+                            <div class="team_img">
+                                <img src="https://i.imgur.com/2Necikc.png" alt="Team_image" />
+                            </div>
+                            <h3>Alex Wood</h3>
+                            <p class="role">Support Lead</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quaerat tempora, voluptatum quas facere dolorum aut cumque nihil nulla harum nemo distinctio quam blanditiis dignissimos.</p>
+                    </div>
+                </div>
+            </div>
         </>
     )
 };
