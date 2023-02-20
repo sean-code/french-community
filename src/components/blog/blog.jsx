80e98ad874b40f1fd43e108ea08b6dffaa39a80e
sean-code <john.nganga@student.moringaschool.com>
2023-02-20 18:34:51 +0300

    Create and style the read mre buttonb for blogs

--- src/components/blog/blog.jsx
+++ src/components/blog/blog.jsx
@@ -10,80 +10,12 @@ const Blog = () => {
         <div className='blogs-header'>
             <h1><b>Read Our Blogs</b></h1>
         </div>
-        <div className='blogs'>
+        <div className='blogs' style={{marginBottom:'40px'}}>
             <CardGroup className='blog-groups'>
                 <Card className='blog-cards'>
-                    <Card.Img className='blog-images' variant="top" src="https://images.unsplash.com/photo-1531482615713-2afd69097998?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8QmxhY2slMjBDb21tdW5pdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" />
+                    <Card.Img className='blog-images' variant="top" src="https://media.istockphoto.com/id/1412793151/photo/container-truck-along-a-scenic-road-through-the-canadian-rockies.jpg?b=1&s=170667a&w=0&k=20&c=GXKtnOeTmD7Thkbko--jTgjCbJ7Iv2C22imVOREeNGU=" />
                     <Card.Body>
-                    <Card.Title style={{textAlign:'center'}}>Team Players</Card.Title>
-                    <Card.Text className='card-text'>
-                        We have a team of professional experienced
-                        translators and interpreters in diverse fields of specialization, for: French to English, French to Swahili, and Vice Versa..
-                    </Card.Text>
-                    </Card.Body>
-                </Card>
-                <Card className='blog-cards'>
-                    <Card.Img  className='blog-images' variant="top" src="https://images.unsplash.com/photo-1546410531-bb4caa6b424d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8TG92ZSUyMHRvJTIwbGVhcm58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" />
-                    <Card.Body>
-                    <Card.Title style={{textAlign:'center'}}>Affordability</Card.Title>
-                    <Card.Text className='card-text'>
-                        We offer the invaluable opportunity to explore the language in real life situations by engaging with French speakers and exploring authentic situations such as presentations, expositions, talents, and music
-                    </Card.Text>
-                    </Card.Body>
-                </Card>
-                <Card className='blog-cards'>
-                    <Card.Img  className='blog-images' variant="top" src="https://media.istockphoto.com/id/892403280/photo/learn-french-education-concept.jpg?b=1&s=170667a&w=0&k=20&c=ixVWpLCWUfFtWei8DtWpogKLC64YPT3YZCFZ8QVKtsY=" />
-                    <Card.Body>
-                    <Card.Title style={{textAlign:'center'}}>Reciprocal Communication</Card.Title>
-                    <Card.Text className='card-text'>
-                    We facilitate language learning and teaching
-                    in different ways as we encourage reciprocal communication between all levels of french in terms of learning and teaching
-                    </Card.Text>
-                    </Card.Body>
-                </Card>
-                <Card className='blog-cards'>
-                    <Card.Img  className='blog-images' variant="top" src="https://media.istockphoto.com/id/892403280/photo/learn-french-education-concept.jpg?b=1&s=170667a&w=0&k=20&c=ixVWpLCWUfFtWei8DtWpogKLC64YPT3YZCFZ8QVKtsY=" />
-                    <Card.Body>
-                    <Card.Title style={{textAlign:'center'}}>Reciprocal Communication</Card.Title>
-                    <Card.Text className='card-text'>
-                    We facilitate language learning and teaching
-                    in different ways as we encourage reciprocal communication between all levels of french in terms of learning and teaching
-                    </Card.Text>
-                    </Card.Body>
-                </Card>
-            </CardGroup>
-            <CardGroup>
-                <Card className='blog-cards'>
-                    <Card.Img  className='blog-images' variant="top" src="https://media.istockphoto.com/id/502641289/photo/hello-hello.jpg?s=612x612&w=0&k=20&c=a6b-fV5WQkx_xUCG-JKUnkHrnf4GDxEepCDKHfW_t7I=" />
-                    <Card.Body>
-                    <Card.Title style={{textAlign:'center'}}>Fun and Interactive</Card.Title>
-                    <Card.Text className='card-text'>
-                        We are fun and interactive. Your French lessons focus on interactions, while still following a thorough mode of learning
-                    </Card.Text>
-                    </Card.Body>
-                </Card>
-                <Card className='blog-cards'>
-                    <Card.Img  className='blog-images' variant="top" src="https://media.istockphoto.com/id/1148799429/photo/learn-francais-word.jpg?s=612x612&w=0&k=20&c=XlBl8ajnZIWUoR0rPSv2FjEwmecxO9-dz98T6djx6_I=" />
-                    <Card.Body>
-                    <Card.Title style={{textAlign:'center'}}>French in French</Card.Title>
-                    <Card.Text className='card-text'>
-                        Learn French by speaking French and not English. This will accelerate your learning process
-                    </Card.Text>
-                    </Card.Body>
-                </Card>
-                <Card className='blog-cards'>
-                    <Card.Img  className='blog-images' variant="top" src="https://media.istockphoto.com/id/899192146/photo/group-of-five-peoples-in-silhouettes-at-sunset.jpg?s=612x612&w=0&k=20&c=7s6FTgu2LSxqTlifcnwC2hLr6vQEOxasAoE-XE2bW4g=" />
-                    <Card.Body>
-                    <Card.Title style={{textAlign:'center'}}>Passionate Community</Card.Title>
-                    <Card.Text className='card-text'>
-                    Join our passionate community and practice your French!
-                    </Card.Text>
-                    </Card.Body>
-                </Card>
-                <Card className='blog-cards'>
-                    <Card.Img  className='blog-images' variant="top" src="https://media.istockphoto.com/id/892403280/photo/learn-french-education-concept.jpg?b=1&s=170667a&w=0&k=20&c=ixVWpLCWUfFtWei8DtWpogKLC64YPT3YZCFZ8QVKtsY=" />
-                    <Card.Body>
-                    <Card.Title style={{textAlign:'center'}}>
+                    <Card.Title style={{textAlign:'center', fontWeight:'600'}}>
                         <LoremIpsum
                             avgWordsPerSentence={2}
                             avgSentencesPerParagraph={1}
@@ -91,10 +23,187 @@ const Blog = () => {
                         />
                     </Card.Title>
                     <Card.Text className='card-text'>
-                    <LoremIpsum 
-                        avgWordsPerSentence={2}
-                    />
+                        <LoremIpsum 
+                            avgWordsPerSentence={2}
+                        />
                     </Card.Text>
+                        <div className='blogs-button'>
+                            <p>
+                                <a href="/#" target="_blank">
+                                    Read More ➤ 
+                                </a>
+                            </p>
+                        </div>
+                    </Card.Body>
+                </Card>
+                <Card className='blog-cards'>
+                    <Card.Img  className='blog-images' variant="top" src="https://media.istockphoto.com/id/1222023932/photo/traveler-on-the-top-of-a-mountain.jpg?b=1&s=170667a&w=0&k=20&c=e8cCy39A1RNgtDs27_j-_N5D6TeAD3i7vwtikPWmitw=" />
+                    <Card.Body>
+                    <Card.Title style={{textAlign:'center', fontWeight:'600'}}>
+                        <LoremIpsum
+                            avgWordsPerSentence={2}
+                            avgSentencesPerParagraph={1}
+                            startWithLoremIpsum="false"
+                        />
+                    </Card.Title>
+                    <Card.Text className='card-text'>
+                        <LoremIpsum 
+                            avgWordsPerSentence={2}
+                        />
+                    </Card.Text>
+                        <div className='blogs-button'>
+                            <p>
+                                <a href="/#" target="_blank">
+                                    Read More ➤ 
+                                </a>
+                            </p>
+                        </div>
+                    </Card.Body>
+                </Card>
+                <Card className='blog-cards'>
+                    <Card.Img  className='blog-images' variant="top" src="https://media.istockphoto.com/id/904559406/photo/whats-that-one-doing.jpg?b=1&s=170667a&w=0&k=20&c=L5So_JmTSDqS-YAuP13LQQEmZ4M_2sqoc39a75pV7js=" />
+                    <Card.Body>
+                    <Card.Title style={{textAlign:'center', fontWeight:'600'}}>
+                        <LoremIpsum
+                            avgWordsPerSentence={2}
+                            avgSentencesPerParagraph={1}
+                            startWithLoremIpsum="false"
+                        />
+                    </Card.Title>
+                    <Card.Text className='card-text'>
+                        <LoremIpsum 
+                            avgWordsPerSentence={2}
+                        />
+                    </Card.Text>
+                        <div className='blogs-button'>
+                            <p>
+                                <a href="/#" target="_blank">
+                                    Read More ➤ 
+                                </a>
+                            </p>
+                        </div>
+                    </Card.Body>
+                </Card>
+                <Card className='blog-cards'>
+                    <Card.Img  className='blog-images' variant="top" src="https://media.istockphoto.com/id/1337232523/photo/high-angle-view-of-a-lake-and-forest.jpg?b=1&s=170667a&w=0&k=20&c=MMQAIpDombUQd_8FV9os3jLzza26-oqaOhO3AgUSt9k=" />
+                    <Card.Body>
+                    <Card.Title style={{textAlign:'center', fontWeight:'600'}}>
+                        <LoremIpsum
+                            avgWordsPerSentence={2}
+                            avgSentencesPerParagraph={1}
+                            startWithLoremIpsum="false"
+                        />
+                    </Card.Title>
+                    <Card.Text className='card-text'>
+                        <LoremIpsum 
+                            avgWordsPerSentence={2}
+                        />
+                    </Card.Text>
+                        <div className='blogs-button'>
+                            <p>
+                                <a href="/#" target="_blank">
+                                    Read More ➤ 
+                                </a>
+                            </p>
+                        </div>
+                    </Card.Body>
+                </Card>
+            </CardGroup>
+            <CardGroup>
+                <Card className='blog-cards'>
+                    <Card.Img  className='blog-images' variant="top" src="https://media.istockphoto.com/id/1393907055/photo/sheep-grazing-in-the-field-in-front-of-mont-saint-michel-in-france.jpg?b=1&s=170667a&w=0&k=20&c=ik1kD3Uj7Xg_DKXZCLR6Iqg6iOmmmx25dkL-Jzjb5mE=" />
+                    <Card.Body>
+                    <Card.Title style={{textAlign:'center', fontWeight:'600'}}>
+                        <LoremIpsum
+                            avgWordsPerSentence={2}
+                            avgSentencesPerParagraph={1}
+                            startWithLoremIpsum="false"
+                        />
+                    </Card.Title>
+                    <Card.Text className='card-text'>
+                        <LoremIpsum 
+                            avgWordsPerSentence={2}
+                        />
+                    </Card.Text>
+                        <div className='blogs-button'>
+                            <p>
+                                <a href="/#" target="_blank">
+                                    Read More ➤ 
+                                </a>
+                            </p>
+                        </div>
+                    </Card.Body>
+                </Card>
+                <Card className='blog-cards'>
+                    <Card.Img  className='blog-images' variant="top" src="https://media.istockphoto.com/id/690416076/photo/asian-little-girl-and-parent-planting-young-tree-on-black-soil-together.jpg?b=1&s=170667a&w=0&k=20&c=j7zmRaZc7ruTOx4BCEUxM6ziQhyjtpD2zrkRXOn4DuY=" />
+                    <Card.Body>
+                    <Card.Title style={{textAlign:'center', fontWeight:'600'}}>
+                        <LoremIpsum
+                            avgWordsPerSentence={2}
+                            avgSentencesPerParagraph={1}
+                            startWithLoremIpsum="false"
+                        />
+                    </Card.Title>
+                    <Card.Text className='card-text'>
+                        <LoremIpsum 
+                            avgWordsPerSentence={2}
+                        />
+                    </Card.Text>
+                        <div className='blogs-button'>
+                            <p>
+                                <a href="/#" target="_blank">
+                                    Read More ➤ 
+                                </a>
+                            </p>
+                        </div>
+                    </Card.Body>
+                </Card>
+                <Card className='blog-cards'>
+                    <Card.Img  className='blog-images' variant="top" src="https://media.istockphoto.com/id/1436430810/photo/paris-eiffel-tower.jpg?b=1&s=170667a&w=0&k=20&c=Qm33k45p4AGKtbNcqkx5hhfP7IRo8RYIpW_VdgE2bDU=" />
+                    <Card.Body>
+                    <Card.Title style={{textAlign:'center', fontWeight:'600'}}>
+                        <LoremIpsum
+                            avgWordsPerSentence={2}
+                            avgSentencesPerParagraph={1}
+                            startWithLoremIpsum="false"
+                        />
+                    </Card.Title>
+                    <Card.Text className='card-text'>
+                        <LoremIpsum 
+                            avgWordsPerSentence={2}
+                        />
+                    </Card.Text>
+                        <div className='blogs-button'>
+                            <p>
+                                <a href="/#" target="_blank">
+                                    Read More ➤ 
+                                </a>
+                            </p>
+                        </div>
+                    </Card.Body>
+                </Card>
+                <Card className='blog-cards'>
+                    <Card.Img  className='blog-images' variant="top" src="https://media.istockphoto.com/id/1127582234/photo/waving-french-flags.jpg?b=1&s=170667a&w=0&k=20&c=9dSeBVT3lY76HtbGrGXsjmKtSx8H-LNNIcj3Ohu1zwY=" />
+                    <Card.Body>
+                    <Card.Title style={{textAlign:'center', fontWeight:'600'}}>
+                        <LoremIpsum
+                            avgWordsPerSentence={2}
+                            avgSentencesPerParagraph={1}
+                            startWithLoremIpsum="false"
+                        />
+                    </Card.Title>
+                    <Card.Text className='card-text'>
+                        <LoremIpsum 
+                            avgWordsPerSentence={2}
+                        />
+                    </Card.Text>
+                        <div className='blogs-button'>
+                            <p>
+                                <a href="/#" target="_blank">
+                                    Read More ➤ 
+                                </a>
+                            </p>
+                        </div>
                     </Card.Body>
                 </Card>
             </CardGroup>
