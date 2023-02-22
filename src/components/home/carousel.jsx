fdd60e8163f3a36f1e22fd9da77504261e73c504
sean-code <john.nganga@student.moringaschool.com>
2023-02-22 14:48:24 +0300

    Add media queries for xs devices

--- src/components/home/carousel.jsx
+++ src/components/home/carousel.jsx
@@ -7,20 +7,20 @@ const Slider = () => {
     return(
         <div className='carousel-page'>
             <Carousel fade indicators={false} controls={false} pause={false} >
-                <Carousel.Item interval={1200}>
+                <Carousel.Item interval={1900}>
                     <img
-                    style={{width:'1%', height:'20%', filter:'brightness(50%)'}}
+                    style={{width:'10%', height:'20%', filter:'brightness(50%)'}}
                     className="d-block w-100"
                     src="https://images.unsplash.com/photo-1582213782179-e0d53f98f2ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y29tbXVuaXR5JTIwc2VydmljZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
                     alt="First slide"
                     />
-                    <Carousel.Caption>
-                    <h1 style={{textAlign:'center'}}>
-                        Join Us Today<br />For a Better Tomorrow</h1>
+                    <Carousel.Caption className='carousel-caption'>
+                    <h1 style={{}}>
+                        Join Us Today For a Better Tomorrow</h1>
                     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item interval={1200}>
+                <Carousel.Item interval={1900}>
                     <img
                      style={{width:'10%', filter:'brightness(50%)'}}
                     className="d-block w-100"
@@ -28,24 +28,23 @@ const Slider = () => {
                     alt="Second slide"
                     />
 
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <Carousel.Caption className='carousel-caption'>
+                    <h1 style={{}}>
+                        Experience Reciprocal Communication</h1>
+                    <p>This includes all levels of french in terms of learning and teaching</p>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item interval={1200}>
+                <Carousel.Item interval={1900}>
                     <img
                      style={{width:'10%', filter:'brightness(50%)'}}
                     className="d-block w-100"
                     src="https://media.istockphoto.com/id/1403441234/photo/cheerful-teenagers-putting-their-hands-together-in-unity.jpg?b=1&s=170667a&w=0&k=20&c=0hmgD9uy3JjjdRLJeEx-9v5acz-DKJOAShd4JlDYmGs="
                     alt="Third slide"
                     />
-
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>
-                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                        </p>
+                    <Carousel.Caption className='carousel-caption'>
+                        <h1 style={{}}>
+                            Our Passionate Brotherhood and Community</h1>
+                        <p>We are highly fun and interactive</p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
